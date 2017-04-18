6fb726f26837476653c7be4805d2c1d1470c4e3e
Christoph Rosemann <christoph.rosemann@desy.de>
2017-04-18 17:28:02 +0200

    minor change

--- liveViewer.py
+++ liveViewer.py
@@ -1,4 +1,3 @@
-#
 # author: Ch. Rosemann, DESY
 # email: christoph.rosemann@desy.de
 
@@ -9,10 +8,6 @@
 
 import pyqtgraph as pg
 import math
-try:
-    import hidra
-except ImportError:
-    print("without hidra installed this does not make sense")
 
 from PyQt4 import QtCore, QtGui
 import sys
@@ -23,77 +18,77 @@ class gui_definition(QtGui.QDialog):
     def __init__(self, parent=None, signal_host=None, target=None):
         super(gui_definition, self).__init__(parent)
 
-        # make a grid layout
-        # | hidra | image path | 
-        # 
         vlayout = QtGui.QVBoxLayout()
         hw = hidra_widget()
-        isw = intensityscaling_widget()
+        self.isw = intensityscaling_widget()
 
         globallayout = QtGui.QHBoxLayout()
-        
+
         self.img_w = image_widget()
-        #~ self.img_w = image_alt_widget()
-        
+
         self.raw_image = None
-        
-        ## define grid elements
+
+        # define grid elements
         vlayout.addWidget(hw)
-        vlayout.addWidget(isw)
-        
+        vlayout.addWidget(self.isw)
+
         globallayout.addLayout(vlayout)
         globallayout.addWidget(self.img_w)
-        
+
         self.setLayout(globallayout)
 
         self.setWindowTitle("Live Image Viewer")
-    
-    
+
+        self.isw.changeScaling.connect(self.replot)
+
     def plot(self, nparr):
         self.raw_image = nparr
-        self.img_w.plot(nparr)
-        
+        self.img_w.plot(nparr, self.isw.getCurrentScaling())
+
+    def replot(self, style):
+        self.img_w.plot(self.raw_image, style)
 
 
 class hidra_widget(QtGui.QWidget):
+
     """
     Connect and disconnect hidra service.
-    """    
-    
+    """
+
     def __init__(self, parent=None, signal_host=None, target=None):
         super(hidra_widget, self).__init__(parent)
-        self.signal_host=signal_host
-        self.target=target
+        self.signal_host = signal_host
+        self.target = target
         self.connected = False
-        # grid/table layout: 
+        # grid/table layout:
         # | label:         | server name/details |
         # | connect button |  status display     |
         gridlayout = QtGui.QGridLayout()
-        
+
         self.widget00 = QtGui.QLabel(u"HiDRA server")
         self.widget01 = QtGui.QLabel(u"SomeName")
         self.widget10 = QtGui.QPushButton("Connect")
         self.widget11 = QtGui.QLineEdit("Not connected")
-        
+
         self.widget10.clicked.connect(self.toggleServerConnection)
-        
+
         gridlayout.addWidget(self.widget00, 0, 0)
         gridlayout.addWidget(self.widget10, 1, 0)
         gridlayout.addWidget(self.widget01, 0, 1)
         gridlayout.addWidget(self.widget11, 1, 1)
-        
+
         self.setLayout(gridlayout)
-        
+
     def toggleServerConnection(self):
         if(not self.connected):
             try:
                 establish_hidra_connection(self.signal_host, self.target)
             except:
                 print("Big big connect error")
-                return 
+                return
 
         self.connected = not self.connected
-        
+
         if(self.connected):
             self.widget11.setText("Connected")
         else:
@@ -101,46 +96,49 @@ class hidra_widget(QtGui.QWidget):
 
 
 class imagesettings_widget(QtGui.QWidget):
+
     """
     Control the image settings.
-    """    
-    
+    """
+
     def __init__(self, parent=None):
         super(imagesettings_widget, self).__init__(parent)
 
-        # two columns layout: 
+        # two columns layout:
         # | radiobuttons       | checkboxes |
-        #columnlayout = QtGui.QHBoxLayout()
-        
+        # columnlayout = QtGui.QHBoxLayout()
+
         #~ leftw = imagetransformations_widget()
         rightw = intensityscaling_widget()
-        
+
         #~ columnlayout.addWidget(leftw)
         #~ columnlayout.addWidget(rightw)
-        
+
         #~ self.setLayout(columnlayout)
 
 
 class intensityscaling_widget(QtGui.QWidget):
+
     """
     Select how the image intensity is supposed to be scaled.
     """
-    
+    changeScaling = QtCore.pyqtSignal(str)
+
     def __init__(self, parent=None):
         super(intensityscaling_widget, self).__init__(parent)
-        
+
         self.current = "sqrt"
         verticallayout = QtGui.QVBoxLayout()
-        
+
         self.linbutton = QtGui.QRadioButton(u"linear")
         self.logbutton = QtGui.QRadioButton(u"log")
         self.sqrtbutton = QtGui.QRadioButton(u"sqrt")
-        
+
         self.linbutton.toggled.connect(self.setCurrentScaling)
         self.logbutton.toggled.connect(self.setCurrentScaling)
         self.sqrtbutton.toggled.connect(self.setCurrentScaling)
         self.sqrtbutton.setChecked(True)
-        
+
         verticallayout.addWidget(self.linbutton)
         verticallayout.addWidget(self.logbutton)
         verticallayout.addWidget(self.sqrtbutton)
@@ -149,30 +147,31 @@ class intensityscaling_widget(QtGui.QWidget):
 
     def getCurrentScaling(self):
         return self.current
-    
+
     def setCurrentScaling(self, scaling):
         if self.linbutton.isChecked():
-            self.current = "lin"
+            self.changeScaling.emit("lin")
         elif self.logbutton.isChecked():
-            self.current = "log"
+            self.changeScaling.emit("log")
         else:
-            self.current = "sqrt"
-        
+            self.changeScaling.emit("sqrt")
+
 
 class imagetransformations_widget(QtGui.QWidget):
+
     """
     Select how an image should be transformed.
     """
-    
+
     def __init__(self, parent=None):
         super(imagetransformations_widget, self).__init__(parent)
-        
+
         verticallayout = QtGui.QVBoxLayout()
-        
+
         flip = QtGui.QCheckBox(u"flip")
         mirror = QtGui.QCheckBox(u"mirror")
         rotate90 = QtGui.QCheckBox(u"rotate90")
-        
+
         verticallayout.addWidget(flip)
         verticallayout.addWidget(mirror)
         verticallayout.addWidget(rotate90)
@@ -181,17 +180,18 @@ class imagetransformations_widget(QtGui.QWidget):
 
 
 class image_widget(QtGui.QWidget):
+
     """
     The part of the GUI that incorporates the image view.
     """
-    
+
     def __init__(self, parent=None):
         super(image_widget, self).__init__(parent)
-        
+
         self.nparray = None
         self.crosshair_locked = False
         self.imageItem = None
-        
+
         # the actual image is an item of the PlotWidget
         self.img_widget = pg.PlotWidget()
         self.img_widget.scene().sigMouseMoved.connect(self.mouse_position)
@@ -200,25 +200,20 @@ class image_widget(QtGui.QWidget):
         self.vLine = pg.InfiniteLine(angle=90, movable=False, pen=(255, 0, 0))
         self.hLine = pg.InfiniteLine(angle=0, movable=False, pen=(255, 0, 0))
 
-        gridlayout = QtGui.QGridLayout()
-        gridlayout.addWidget(self.img_widget, 0,0)
+        verticallayout = QtGui.QVBoxLayout()
 
-        # the vertical projection on one side
-        self.img_vproj_widget = pg.PlotWidget()
-        gridlayout.addWidget(self.img_vproj_widget, 0,1)
+        self.filenamedisplay = QtGui.QLineEdit()
+        verticallayout.addWidget(self.filenamedisplay)
 
-        # the horizontal projection below
-        self.img_hproj_widget = pg.PlotWidget()
-        gridlayout.addWidget(self.img_hproj_widget, 1,0)
+        verticallayout.addWidget(self.img_widget)
 
         self.infodisplay = QtGui.QLineEdit()
-        gridlayout.addWidget(self.infodisplay, 1,1)
+        verticallayout.addWidget(self.infodisplay)
 
-        self.setLayout(gridlayout)
-        
+        self.setLayout(verticallayout)
 
     def mouse_position(self, event):
-        
+
         try:
             mousePoint = self.imageItem.mapFromScene(event)
             xdata = math.floor(mousePoint.x())
@@ -230,12 +225,12 @@ class image_widget(QtGui.QWidget):
 
             intensity = self.nparray[math.floor(xdata), math.floor(ydata)]
             self.infodisplay.setText("x=%.2f, y=%.2f, intensity=%.2f"
-                                           % (xdata, ydata, intensity))
+                                     % (xdata, ydata, intensity))
         except:
             self.infodisplay.setText("error")
 
     def mouse_click(self, event):
-        
+
         mousePoint = self.imageItem.mapFromScene(event.scenePos())
 
         xdata = mousePoint.x()
@@ -247,40 +242,39 @@ class image_widget(QtGui.QWidget):
             if not self.crosshair_locked:
                 self.vLine.setPos(xdata)
                 self.hLine.setPos(ydata)
-             
-    def plot(self, nparr):
+
+    def plot(self, nparr, style):
         self.nparray = nparr
-        
+        if style == "lin":
+            drawarray = nparr
+        elif style == "log":
+            drawarray = np.log10(nparr)
+        elif style == "sqrt":
+            drawarray = np.sqrt(nparr)
+        else:
+            print("Chosen display style '" + style + "' is not valid.")
+            return
+
         if self.imageItem is None:
-            self.imageItem = pg.ImageItem(self.nparray)
+            self.imageItem = pg.ImageItem(drawarray)
             self.img_widget.addItem(self.imageItem)
             self.img_widget.addItem(self.vLine, ignoreBounds=True)
             self.img_widget.addItem(self.hLine, ignoreBounds=True)
         else:
-            self.imageItem.setImage(self.nparray)
-            
-
-def establish_hidra_connection(signal_host=None, target=None):
-    try:
-        query = hidra.Transfer("QUERY_NEXT", signal_host)
-        query.initiate(target)
-        query.start()
-    except:
-        query.stop()	# remove list entry established by query.initiate()
-        sys.exit(255)	# exit - should be replaced by retrying query with other port
-
+            self.imageItem.setImage(drawarray, autoLevels=True)
 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    
-    dialog = gui_definition(signal_host = "haspp03pilatus.desy.de", target = [["haspp03.desy.de", "50111", 0, [".cbf"]]])
-    #dialog = image_widget()
-    
+
+    dialog = gui_definition(signal_host="haspp03pilatus.desy.de",
+                            target=[["haspp03.desy.de", "50111", 0, [".cbf"]]])
+    # dialog = image_widget()
+
     # to take out: generate random image
     import numpy as np
-    rand_arr = np.random.rand(550,550)
-            
+    rand_arr = np.random.rand(550, 550)
+
     dialog.plot(rand_arr)
     #~ dialog = hidra_widget()
     dialog.show()
