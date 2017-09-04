1966cbc9d37a1b3e34cd172e1a90090980f7aa2c
Christoph Rosemann <christoph.rosemann@desy.de>
2017-09-04 16:04:27 +0200

    make the handling of image files abstract

---
+++ lavue/imageFileHandler.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2017  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# email contact: christoph.rosemann@desy.de
+#
+# lavue is an image viewing program for photon science imaging detectors.
+# Its usual application is as a live viewer using hidra as data source.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation in  version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+# this a simple file handler that loads image files
+# and delivers just the actual array
+
+import fabio
+
+class ImageFileHandler():
+    '''Simple file handler class.
+       Reads image from file and returns the numpy array.'''
+
+    def __init__(self, fname):
+        self._image = fabio.open(fname)
+        self._data = self._image.data
+
+    def getImage(self):
+        return self._data
