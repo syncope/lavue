6481b11d398b06954641fa9b8ce3307538af1f16
Christoph Rosemann <christoph.rosemann@desy.de>
2017-09-07 16:42:16 +0200

    most pragmatic solution to display small values in log mode: re-set the minimum allowed values in the checkbox

--- lavue/levelsWidget.py
+++ lavue/levelsWidget.py
@@ -53,9 +53,9 @@ class LevelsWidget(QtGui.QGroupBox):
         self.maxVal = 1.
 
         self.minValSB = QtGui.QDoubleSpinBox()
-        self.minValSB.setMinimum(0.)
+        self.minValSB.setMinimum(-2.)
         self.maxValSB = QtGui.QDoubleSpinBox()
-        self.maxValSB.setMinimum(1.)
+        self.maxValSB.setMinimum(0.)
         self.maxValSB.setMaximum(10e20)
         self.applyButton = QtGui.QPushButton("Apply levels")
 
