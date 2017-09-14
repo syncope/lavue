8348ed02fff1da5b1bd53e21d93b847c1d4e7ea3
Christoph Rosemann <christoph.rosemann@desy.de>
2017-09-14 14:38:15 +0200

    + ! forgot to include another file!

---
+++ lavue/preparationBoxWidget.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2017  Christoph Rosemann, DESY, Notkestr. 85, D-22607 Hamburg
+# email contact: christoph.rosemann@desy.de
+#
+# lavue is an image viewing program for photon science imaging detectors.
+# Its usual application is as a live viewer using hidra as data source.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation in  version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+
+from PyQt4 import QtCore, QtGui
+
+from . import transformationsWidget
+#~ from . import maskWidget
+from . import bkgSubtractionWidget
+
+    
+class QHLine(QtGui.QFrame):
+    def __init__(self):
+        super(QHLine, self).__init__()
+        self.setFrameShape(QtGui.QFrame.HLine)
+        self.setFrameShadow(QtGui.QFrame.Sunken)
+
+
+class PreparationBoxWidget(QtGui.QGroupBox):
+    
+    def __init__(self, parent=None):
+        super(PreparationBoxWidget, self).__init__(parent)
+        self.setTitle("Image preparation")
+
+        #~ self.maskW = maskWidget.MaskWidget(parent=self)
+        self.bkgSubW = bkgSubtractionWidget.BkgSubtractionkWidget(parent=self)
+
+        hline = QHLine()
+
+        self.trafoW = transformationsWidget.TransformationsWidget(parent=self)
+
+        vlayout = QtGui.QVBoxLayout()
+        #~ vlayout.addWidget(self.maskW)
+        vlayout.addWidget(self.bkgSubW)
+        vlayout.addWidget(hline)
+        vlayout.addWidget(self.trafoW)
+
+        self.setLayout(vlayout)
