c087e3b80b3380b8005dd2514c7954ce57b82d71
Jan Kotanski <jankotan@gmail.com>
2018-01-24 17:13:15 +0100

    add GeometryWidget.ui

--- lavuelib/geometryWidget.py
+++ lavuelib/geometryWidget.py
@@ -25,7 +25,8 @@
 
 """ detector geometry widget """
 
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui, QtCore, uic
+import os
 
 
 class GeometryWidget(QtGui.QDialog):
@@ -34,9 +35,14 @@ class GeometryWidget(QtGui.QDialog):
 
     def __init__(self, parent=None):
         """ constructor
-        """
 
+        :param parent: parent object
+        :type parent: :class:`PyQt4.QtCore.QObject`
+        """
         QtGui.QDialog.__init__(self, parent)
+        self.__ui = uic.loadUi(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         "ui", "GeometryWidget.ui"), self)
 
         #: (:obj:`float`) x-coordinates of the center of the image
         self.centerx = 0.0
@@ -51,134 +57,70 @@ class GeometryWidget(QtGui.QDialog):
         #: (:obj:`float`) detector distance in mm
         self.detdistance = 0.0
 
-        self.centerxLineEdit = None
-        self.centeryLineEdit = None
-        self.energyLineEdit = None
-        self.pixelsizexLineEdit = None
-        self.pixelsizeyLineEdit = None
-        self.detdistanceLineEdit = None
-
     def createGUI(self):
         """ create GUI
         """
-
-        self.setWindowTitle("Geometry")
-
-        gridlayout = QtGui.QGridLayout()
-        vlayout = QtGui.QVBoxLayout()
-
-        centerxLabel = QtGui.QLabel(u"Center X [pixels]:")
-        centerxLabel.setToolTip(
-            " x-coordinates of the center of the image in pixels")
-        self.centerxLineEdit = QtGui.QLineEdit(str(self.centerx))
-        self.centerxLineEdit.setToolTip(
-            " x-coordinates of the center of the image in pixels")
-
-        centeryLabel = QtGui.QLabel(u"Center Y [pixels]:")
-        centeryLabel.setToolTip(
-            " y-coordinates of the center of the image in pixels")
-        self.centeryLineEdit = QtGui.QLineEdit(str(self.centery))
-        self.centeryLineEdit.setToolTip(
-            " y-coordinates of the center of the image in pixels")
-
-        energyLabel = QtGui.QLabel(u"Energy [eV]:")
-        energyLabel.setToolTip("light energy in eV")
-        self.energyLineEdit = QtGui.QLineEdit(str(self.energy))
-        self.energyLineEdit.setToolTip("light energy in eV")
-
-        pixelsizexLabel = QtGui.QLabel(u"Pixel x-size [\u00B5m]:")
-        pixelsizexLabel.setToolTip("pixel x-size in microns")
-        self.pixelsizexLineEdit = QtGui.QLineEdit(str(self.pixelsizex))
-        self.pixelsizexLineEdit.setToolTip("pixel x-size in microns")
-
-        pixelsizeyLabel = QtGui.QLabel(u"Pixel y-size [\u00B5m]:")
-        pixelsizeyLabel.setToolTip("pixel y-size in microns")
-        self.pixelsizeyLineEdit = QtGui.QLineEdit(str(self.pixelsizey))
-        self.pixelsizeyLineEdit.setToolTip("pixel y-size in microns")
-
-        detdistanceLabel = QtGui.QLabel(u"Detector distance [mm]:")
-        detdistanceLabel.setToolTip("Detector distance in mm")
-        self.detdistanceLineEdit = QtGui.QLineEdit(str(self.detdistance))
-        self.detdistanceLineEdit.setToolTip("Detector distance in mm")
-
-        gridlayout.addWidget(centerxLabel, 0, 0)
-        gridlayout.addWidget(self.centerxLineEdit, 0, 1)
-        gridlayout.addWidget(centeryLabel, 1, 0)
-        gridlayout.addWidget(self.centeryLineEdit, 1, 1)
-        gridlayout.addWidget(energyLabel, 2, 0)
-        gridlayout.addWidget(self.energyLineEdit, 2, 1)
-        gridlayout.addWidget(pixelsizexLabel, 3, 0)
-        gridlayout.addWidget(self.pixelsizexLineEdit, 3, 1)
-        gridlayout.addWidget(pixelsizeyLabel, 4, 0)
-        gridlayout.addWidget(self.pixelsizeyLineEdit, 4, 1)
-        gridlayout.addWidget(detdistanceLabel, 5, 0)
-        gridlayout.addWidget(self.detdistanceLineEdit, 5, 1)
-
-        self.buttonBox = QtGui.QDialogButtonBox(
-            QtGui.QDialogButtonBox.Ok
-            | QtGui.QDialogButtonBox.Cancel)
-        vlayout.addLayout(gridlayout)
-        vlayout.addWidget(self.buttonBox)
-        self.setLayout(vlayout)
-        self.buttonBox.button(
-            QtGui.QDialogButtonBox.Cancel).clicked.connect(self.reject)
-        self.buttonBox.button(
-            QtGui.QDialogButtonBox.Ok).clicked.connect(self.accept)
+        self.__ui.centerxLineEdit.setText(str(self.centerx))
+        self.__ui.centeryLineEdit.setText(str(self.centery))
+        self.__ui.energyLineEdit.setText(str(self.energy))
+        self.__ui.pixelsizexLineEdit.setText(str(self.pixelsizex))
+        self.__ui.pixelsizeyLineEdit.setText(str(self.pixelsizey))
+        self.__ui.detdistanceLineEdit.setText(str(self.detdistance))
 
     @QtCore.pyqtSlot()
     def accept(self):
         """ updates class variables with the form content
         """
         try:
-            self.centerx = float(self.centerxLineEdit.text())
+            self.centerx = float(self.__ui.centerxLineEdit.text())
         except:
-            self.centerxLineEdit.setFocus()
+            self.__ui.centerxLineEdit.setFocus()
             return
 
         try:
-            self.centery = float(self.centeryLineEdit.text())
+            self.centery = float(self.__ui.centeryLineEdit.text())
         except:
-            self.centeryLineEdit.setFocus()
+            self.__ui.centeryLineEdit.setFocus()
             return
 
         try:
-            energy = float(self.energyLineEdit.text())
+            energy = float(self.__ui.energyLineEdit.text())
             if energy <= 0:
                 raise Exception("Wrong value")
             else:
                 self.energy = energy
         except:
-            self.energyLineEdit.setFocus()
+            self.__ui.energyLineEdit.setFocus()
             return
 
         try:
-            pixelsizex = float(self.pixelsizexLineEdit.text())
+            pixelsizex = float(self.__ui.pixelsizexLineEdit.text())
             if pixelsizex <= 0:
                 raise Exception("Wrong value")
             else:
                 self.pixelsizex = pixelsizex
         except:
-            self.pixelsizexLineEdit.setFocus()
+            self.__ui.pixelsizexLineEdit.setFocus()
             return
 
         try:
-            pixelsizey = float(self.pixelsizeyLineEdit.text())
+            pixelsizey = float(self.__ui.pixelsizeyLineEdit.text())
             if pixelsizey <= 0:
                 raise Exception("Wrong value")
             else:
                 self.pixelsizey = pixelsizey
         except:
-            self.pixelsizeyLineEdit.setFocus()
+            self.__ui.pixelsizeyLineEdit.setFocus()
             return
 
         try:
-            detdistance = float(self.detdistanceLineEdit.text())
+            detdistance = float(self.__ui.detdistanceLineEdit.text())
             if detdistance <= 0:
                 raise Exception("Wrong value")
             else:
                 self.detdistance = detdistance
         except:
-            self.detdistanceLineEdit.setFocus()
+            self.__ui.detdistanceLineEdit.setFocus()
             return
 
         QtGui.QDialog.accept(self)
