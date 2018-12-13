e2f874c44aed398f77636be3252df9fc85d38e7f
Jan Kotanski <jankotan@gmail.com>
2018-12-13 17:41:33 +0100

    add tests

--- test/LavueController_test.py
+++ test/LavueController_test.py
@@ -16,7 +16,7 @@ import subprocess
 import time
 import unittest
 import PyTango
-# import numpy as np
+import numpy as np
 
 # Path
 path = os.path.join(os.path.dirname(__file__), os.pardir)
@@ -137,41 +137,130 @@ class LavueControllerTest(unittest.TestCase):
         """Test for State"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        self.assertEqual(self.proxy.State(), PyTango.DevState.ON)
 
     def test_Status(self):
         """Test for Status"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        self.assertEqual(self.proxy.Status(), 'State is ON')
 
     def test_BeamCenterX(self):
         """Test for BeamCenterX"""
         print("Run: %s.%s() " % (
-            self.__class__.__name__, sys._getframe().f_code.co_name))
+            self.__class__.__name__, sys._getframe().f_code.co_name))        
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+        for wvl in testvalues:
+            self.proxy.BeamCenterX = wvl
+            rvl = self.proxy.BeamCenterX
+            self.assertEqual(wvl, rvl)
 
     def test_BeamCenterY(self):
         """Test for BeamCenterY"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+        for wvl in testvalues:
+            self.proxy.BeamCenterY = wvl
+            rvl = self.proxy.BeamCenterY
+            self.assertEqual(wvl, rvl)
 
     def test_DetectorDistance(self):
         """Test for DetectorDistance"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+        for wvl in testvalues:
+            self.proxy.DetectorDistance = wvl
+            rvl = self.proxy.DetectorDistance
+            self.assertEqual(wvl, rvl)
 
     def test_DetectorROIs(self):
         """Test for DetectorROIs"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [
+            ['{"Pilatus": [[61, 91, 83, 146], [332, 93, 382, 141], '
+             '[116, 69, 279, 94], [91, 155, 157, 199]]}',
+             [["PilatusROI",
+               [61, 91, 83, 146, 332, 93, 382, 141,
+                116, 69, 279, 94, 91, 155, 157, 199]]]],
+            ['{"Pilatus1": [[61, 91, 83, 146], [332, 93, 382, 141], '
+             '[116, 69, 279, 94]], "Pilatus2": [[91, 155, 157, 199]]}',
+             [["Pilatus1ROI",
+               [61, 91, 83, 146, 332, 93, 382, 141,
+                116, 69, 279, 94]]],
+             [["Pilatus2ROI",
+               [ 91, 155, 157, 199]]]],
+            ['{"lambda": [[61, 91, 83, 146]]}',
+             [["LambdaROI",
+               [61, 91, 83, 146]]]],
+            ['{"lambda2": [[61, 91, 83, 146], [1, 21, 33, 146]]}',
+             [["Lambda2ROI",
+               [61, 91, 83, 146, 1, 21, 33, 146]]]],
+            ['{"__null__": [[61, 91, 83, 146]]}',
+             [["ROI",
+               [61, 91, 83, 146]]]],
+            ['{"__null__": [[61, 91, 83, 146], [1, 21, 33, 146]]}',
+             [["ROI",
+               [61, 91, 83, 146, 1, 21, 33, 146]]]],
+        ]
+        for wvl in testvalues:
+            self.proxy.DetectorROIs = str(wvl[0])
+            rvl = self.proxy.DetectorROIs
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                rvl = self.proxy.read_attribute(at).value
+                self.assertTrue(np.array_equal(np.array(vl), rvl))
+            
 
     def test_DetectorROIsValues(self):
         """Test for DetectorROIsValues"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [
+            ['{"Pilatus": [1.23, 12.321, 83.323, 146.32]}',
+             [["PilatusSums",
+               [1.23, 12.321, 83.323, 146.32]]]],
+            ['{"Pilatus1": [1.23, 12.321, 83.323], '
+             '"Pilatus2": [146.32]}',
+             [["Pilatus1Sums",
+               [1.23, 12.321, 83.323]]],
+             [["Pilatus2Sum",
+               [146.32]]]],
+            ['{"lambda": [16.1]}',
+             [["LambdaSum",
+               [16.1]]]],
+            ['{"lambda2": [1231.61, 14.6]}',
+             [["Lambda2Sums",
+               [1231.61, 14.6]]]],
+            ['{"__null__": [12323.0]}',
+             [["Sum",
+               [12323.]]]],
+            ['{"__null__": [12312.0, 1232131.0]}',
+             [["Sums",
+               [12312., 1232131.]]]],
+        ]
+        for wvl in testvalues:
+            self.proxy.DetectorROIsValues = str(wvl[0])
+            rvl = self.proxy.DetectorROIsValues
+            self.assertEqual(wvl[0], rvl)
+            for at, vl in wvl[1]:
+                rvl = self.proxy.read_attribute(at).value
+                if at.endswith("Sums"):
+                    self.assertTrue(np.array_equal(np.array(vl), rvl))
+                else:
+                    self.assertEqual(vl[0], rvl)
 
     def test_Energy(self):
         """Test for Energy"""
         print("Run: %s.%s() " % (
             self.__class__.__name__, sys._getframe().f_code.co_name))
+        testvalues = [12.3, 123.2, -3.43, 0., 23423.]
+        for wvl in testvalues:
+            self.proxy.Energy = wvl
+            rvl = self.proxy.Energy
+            self.assertEqual(wvl, rvl)
 
 
 def main():
