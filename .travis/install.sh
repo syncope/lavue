16210e3aecd5de83c2c6c0c54a3704a52617af90
Jan Kotanski <jankotan@gmail.com>
2020-04-08 09:27:33 +0200

    remove qt4 from tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -18,7 +18,7 @@ then
     exit -1
 fi
 echo "install tango servers"
-docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java libqt4-dev-bin pyqt5-dev-tools'
+docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java pyqt5-dev-tools'
 if [ $? -ne "0" ]
 then
     exit -1
