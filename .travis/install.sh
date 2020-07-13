0ada27c8e575b9d555d3e775e0e0af61fb5b144a
Jan Kotanski <jan.kotanski@desy.de>
2020-07-13 12:19:29 +0200

    use new pyzmq package in all tests (#326)
    * fix zmq alias name
    
    * fix zmq alias name
    
    * allow host:port/topic syntax in zmq configuraion
    
    * add zmq pickle test
    
    * add zmq  test
    
    * impove tests
    
    * fix tests
    
    * change zmq title to bytes in tests
    
    * fix loading pickles in ZMQ source
    
    * fix loading json in tests
    
    * move socket to the thread
    
    * change test port
    
    * add socket closing in tests
    
    * add mutex for socket
    
    * comment unbind
    
    * install new version of pyzmq for tests
    
    * add pyzmq
    
    * add git
    
    * add qt4-qtconfig
    
    * use new pyzmq buster package in tests
    
    * use new pyzmq package in all tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -29,21 +29,13 @@ docker exec -it --user root ndts service tango-starter restart
 
 if [ "$2" = "2" ]; then
     echo "install python packages"
-    if [ "$1" = "debian10" ]; then
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-tz python-pyqtgraph python-setuptools python-zmq python-scipy python-tango'
-    else
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python-pytango python-tz python-pyqtgraph python-setuptools python-zmq python-scipy  libzmq3-dev cython git'
-	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/zeromq/pyzmq pyzmq-src'
-	docker exec -it --user root ndts /bin/sh -c 'cd pyzmq-src; python setup.py install'
-    fi
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-tz python-pyqtgraph python-setuptools python-zmq python-scipy python-pytango'
 else
     echo "install python3 packages"
     if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] ; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-tango'
     else
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy libzmq3-dev cython3  python3-pytango git'
-	docker exec -it --user root ndts /bin/sh -c 'git clone https://github.com/zeromq/pyzmq pyzmq-src'
-	docker exec -it --user root ndts /bin/sh -c 'cd pyzmq-src; python3 setup.py install'
+	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-pytango'
     fi
 fi
 if [ "$?" -ne "0" ]
