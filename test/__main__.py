36f65b96808e590f6aa941442fc07574ffc9d7f8
Jan Kotanski <jan.kotanski@desy.de>
2020-07-23 12:53:11 +0200

    add tests for NeXus File image source (#331)

--- test/__main__.py
+++ test/__main__.py
@@ -98,6 +98,7 @@ if H5CPP_AVAILABLE:
     import H5CppWriter_test
     import FileWriterH5Cpp_test
     import CommandLineArgumentH5Cpp_test
+    import NXSFileImageSource_test
 if PNI_AVAILABLE and H5PY_AVAILABLE:
     import FileWriterPNIH5PY_test
 # if PNI_AVAILABLE and H5Cpp_AVAILABLE:
@@ -132,6 +133,9 @@ def main():
         suite.addTests(
             unittest.defaultTestLoader.loadTestsFromModule(H5PYWriter_test))
     if H5CPP_AVAILABLE:
+        suite.addTests(
+            unittest.defaultTestLoader.loadTestsFromModule(
+                NXSFileImageSource_test))
         suite.addTests(
             unittest.defaultTestLoader.loadTestsFromModule(
                 CommandLineArgumentH5Cpp_test))
