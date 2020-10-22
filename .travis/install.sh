8d85e77598b9a79d1b42f427b5034ef6549b6c57
Jan Kotanski <jan.kotanski@desy.de>
2020-10-22 14:21:28 +0200

    add debian11 tests  (#409)
    * add debian11 tests
    
    * add -y
    
    * change buster to bullseye
    
    * install base-passwd
    
    * remove force yes
    
    * remove force yes
    
    * update package
    
    * update
    
    * update
    
    * update
    
    * change qt4 to qt5
    
    * split tests for deb11
    
    * comment duplicated tests
    
    * pass result via file
    
    * add all_splitted
    
    * add checks

--- .travis/install.sh
+++ .travis/install.sh
@@ -35,7 +35,7 @@ if [ "$2" = "2" ]; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-tz python-pyqtgraph python-setuptools python-zmq python-scipy python-pytango'
 else
     echo "install python3 packages"
-    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] ; then
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "debian11" ] ; then
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-tango'
     else
 	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python3-tz python3-pyqtgraph python3-setuptools python3-zmq python3-scipy python3-pytango'
--- .travis/run.sh
+++ .travis/run.sh
@@ -6,7 +6,7 @@ if [ "$2" = "2" ]; then
 else
     echo "run python3-lavue tests"
     # workaround for pyfai docker problem, return I/O error status=74
-    if [ "$3" = "ubuntu20.04" ]; then
+    if [ "$3" = "ubuntu20.04" ] || [ "$3" = "debian11" ] ; then
 	if [ "$1" = "basic" ]; then
 	    docker exec -it ndts sh -c 'python3 test/__main__.py basic; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 	elif [ "$1" = "controller" ]; then
@@ -17,6 +17,14 @@ else
 	    docker exec -it ndts sh -c 'python3 test/__main__.py tangosource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 	elif [ "$1" = "all" ]; then
 	    docker exec -it ndts sh -c 'python3 test/__main__.py all; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+	elif [ "$1" = "all_splitted" ]; then
+	    docker exec -it ndts sh -c 'python3 test/__main__.py basic; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+	    if [ "$?" -ne "0" ]; then exit -1; fi
+	    docker exec -it ndts sh -c 'python3 test/__main__.py controller; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+	    if [ "$?" -ne "0" ]; then exit -1; fi
+	    docker exec -it ndts sh -c 'python3 test/__main__.py controller2; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
+	    if [ "$?" -ne "0" ]; then exit -1; fi
+	    docker exec -it ndts sh -c 'python3 test/__main__.py tangosource; status=$?; teststatus=$(cat "testresult.txt") && echo "Exit status: $status, Test Result: $teststatus" && exit $teststatus'
 	fi
     else
 	docker exec -it ndts python3 test/__main__.py $1
