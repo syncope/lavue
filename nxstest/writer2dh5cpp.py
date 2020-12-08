ee414982c835ced986c3c5639dcd71d9b213b012
Jan Kotanski <jankotan@gmail.com>
2020-12-08 15:41:42 +0100

    update an example

--- nxstest/writer2dh5cpp.py
+++ nxstest/writer2dh5cpp.py
@@ -1,17 +1,15 @@
 from pninexus import h5cpp
 import numpy as np
 import time
-import sys
 
-if sys.version_info > (3,):
-    unicode = str
 
-readonly = True
-libver = None
+# a new file name
 filename = "swmr.h5"
+
+# data, their shape and chunk
+arr = np.array([[1, 2, 3], [4, 5, 6]])
 shape = [0, 3, 2]
 chunk = [1, 3, 2]
-arr = np.array([[1, 2, 3], [4, 5, 6]])
 
 
 # set the SWMRWRITE and TR UNCATE flags
