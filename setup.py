d6df84116561ee73a69f58d2087705e8fd01262d
Jan Kotanski <jankotan@gmail.com>
2021-10-19 09:32:26 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -224,6 +224,8 @@ SETUPDATA = dict(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='live viewer photon science detector',
     packages=find_packages(exclude=['test']),
