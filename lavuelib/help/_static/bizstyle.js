7c1a5b91d7c7346e7d6f8b0f22644e906b27578e
Jan Kotanski <jankotan@gmail.com>
2022-02-02 11:02:26 +0100

    Issue 636: bump the version to v2,73.0 (#637)
    * bump the version
    
    * add v2.73.0 to documentation

--- lavuelib/help/_static/bizstyle.js
+++ lavuelib/help/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("LaVue (latest) documentation");
+        $("li.nav-item-0 a").text("LaVue 2.73.0 documentation");
     }
 });
