305344920a3e1f18537e6eacf9b82b4fc9f36bee
Jan Kotanski <jan.kotanski@desy.de>
2022-03-21 11:24:49 +0100

    add np.bool_ to numpy encoder (#644)

--- lavuelib/sardanaUtils.py
+++ lavuelib/sardanaUtils.py
@@ -95,8 +95,10 @@ class numpyEncoder(json.JSONEncoder):
             return int(obj)
         elif isinstance(obj, np.floating):
             return float(obj)
-        elif isinstance(obj, np.ndarray) and obj.ndim > 0:
+        elif isinstance(obj, np.ndarray):
             return obj.tolist()
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
         return json.JSONEncoder.default(self, obj)
 
 
